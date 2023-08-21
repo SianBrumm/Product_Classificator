7045e4298a6dbd765dd5519d78ab828c6609f351
Sian Brumm <s17d50umwelt-campus.de>
2023-08-21 19:03:10 +0200

    changed json

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ setup(
     name='product-classificator',
     version='0.1',
     packages=find_packages(),
-    package_data={
-        'product-classificator': ['data/*.json']
-    }
+    package_data={'classificator': ['data/*.json']}
 )
